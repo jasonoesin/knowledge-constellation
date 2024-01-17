4dabb545e9ee060a7033d140631b0fef2347c0a4
Jason Oesin <jasonoesin@gmail.com>
2024-01-18 01:39:06 +0700

    Install Azure OpenAI Services Library

--- backend/src/gql/type-defs.ts
+++ backend/src/gql/type-defs.ts
@@ -3,4 +3,9 @@ export const typeDefs = `#graphql
         title: String
         actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
     }
+
+    type Actor {
+        name: String
+        movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+    }
 `;
