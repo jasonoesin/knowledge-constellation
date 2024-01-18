e1ac2bb335315ba1baedb039be640553e83d1e8e
Jason Oesin <jasonoesin@gmail.com>
2024-01-18 18:32:20 +0700

    Update type-defs.ts

--- backend/src/gql/type-defs.ts
+++ backend/src/gql/type-defs.ts
@@ -1,12 +1,13 @@
 export const typeDefs = `#graphql
     type Graph{
         title: String!
-        clusters: [Cluster!]! @relationship(type: "CREATED", direction: IN)
+        clusters: [Cluster!]! @relationship(type: "BELONGS_TO", direction: IN)
     }
     
     type Cluster{
         name: String!
         keywords: [Keyword!]! @relationship(type: "BELONGS_TO", direction: IN)
+        graph: Graph! @relationship(type: "BELONGS_TO", direction: OUT)
     }
 
     type Keyword{
